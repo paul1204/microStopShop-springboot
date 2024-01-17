211d55d91b67b52b11124bf2f814b7b9318f34a5
Paul <paulsingh4118@gmail.com>
2024-01-16 22:35:31 -0500

    End Point for All Supply - Inventory Service

--- inventory_service/src/main/java/com/paul1204/inventoryservice/controller/InventoryController.java
+++ inventory_service/src/main/java/com/paul1204/inventoryservice/controller/InventoryController.java
@@ -1,10 +1,14 @@
 package com.paul1204.inventoryservice.controller;
 
+import com.paul1204.inventoryservice.model.Inventory;
 import com.paul1204.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -19,9 +23,15 @@ public class InventoryController {
     return inventoryService.isInStock(skuCode);
     }
 
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> getAllSupply(){
+        return inventoryService.getAllSupply();
+    }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<String> deleteStock(@PathVariable("id") Long id){
+    public ResponseEntity<?> deleteStock(@PathVariable("id") Long id){
         return inventoryService.deleteStock(id);
     }
 }
--- inventory_service/src/main/java/com/paul1204/inventoryservice/service/InventoryService.java
+++ inventory_service/src/main/java/com/paul1204/inventoryservice/service/InventoryService.java
@@ -1,11 +1,15 @@
 package com.paul1204.inventoryservice.service;
 
+import com.paul1204.inventoryservice.model.Inventory;
 import com.paul1204.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,4 +26,21 @@ public class InventoryService {
         inventoryRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body("Successfully deleted stock with ID: " + id);
     }
-}
+    public ResponseEntity<?> getAllSupply(){
+        //public ResponseEntity<List<YourObject>> getListOfObjects() {
+            // Assuming YourObject is your data model class
+            Optional<List<Inventory>> optionalList = Optional.of(inventoryRepository.findAll());
+
+        return optionalList.map(objects -> {
+            if (!objects.isEmpty()) {
+                return new ResponseEntity<>(objects, HttpStatus.OK);
+            } else {
+                // If the list is empty, you can return HttpStatus.NO_CONTENT
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        }).orElse(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)); // Handle unexpected null case
+    }
+    }
+
+
+//}
