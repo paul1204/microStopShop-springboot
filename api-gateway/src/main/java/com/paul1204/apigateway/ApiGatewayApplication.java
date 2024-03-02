82c75dc2d7063e123a2c16ac6e12ec56db64f677
Paul <paulsingh4118@gmail.com>
2024-03-02 17:00:02 -0500

    Implemented API Gateway for servies

---
+++ api-gateway/src/main/java/com/paul1204/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.paul1204.apigateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+}
